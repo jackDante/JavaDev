7fdaff857756c78188d949c28b9a8fa709586c16
Giacomo Usai <44863491+jackDante@users.noreply.github.com>
2024-07-01 08:43:23 +0200

    Update Scanner_next.java

--- Scanner_next.java
+++ Scanner_next.java
@@ -8,10 +8,15 @@ public class Scanner_next {
         Double d = scan.nextDouble();
         /*
         Scanner doesn't read whole sentence - difference between 
-        next() and nextLine() of scanner class
+        next() and nextLine() of scanner class. 
+        After supplying data for int, we would hit the enter key. 
+        
+        What nextInt() and nextDouble() does is it assigns integer to appropriate variable and keeps the enter key unread in thekeyboard buffer. 
+        So when its time to supply String the nextLine() will read the enter key from the user thinking that the user has entered the enter key.
+        (that's we get empty output) . Unlike C, there is no fflush() to clean buffer, so we have to flush by not taking it in variable.
         */
-        String s = scan.next();
-        s += scan.nextLine();
+        String s = scan.nextLine();
+        s = scan.nextLine();
         // Write your code here.
 
         System.out.println("String: " + s);
